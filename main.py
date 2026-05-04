8dae7677d77992372a41e0863024247975ab9019
dchavarria3191-dotcom <dchavarri3191@gmail.com>
2026-05-04 17:36:41 -0500

    feet: root, home, index

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route("/") # Controlador
+def root():
+    return render_template(template_name_or_list="base.html") # Vista
+
 @app.route("/index") # Controlador
-def home():
+def index():
     base_de_datos = ["Adrian","Karen","Thanitos"] # Modelos o BD
     return render_template(template_name_or_list="index.html", datos=base_de_datos) # Vista
+
+
+@app.route("/home") # Controlador
+def home():
+    return render_template(template_name_or_list="home.html") # Vista
