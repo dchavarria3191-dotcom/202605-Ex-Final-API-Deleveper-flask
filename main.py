6bf41ee3bcc727347e215c2f2c8021ee2c7511ee
dchavarria3191-dotcom <dchavarri3191@gmail.com>
2026-05-04 16:48:44 -0500

    Version 12

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-base_de_datos = ["Adrian","Karen","Thanitos"] # Modelos o BD
-
 
 @app.route("/index") # Controlador
 def home():
-    return render_template(template_name_or_list="index.html") # Vista
+    base_de_datos = ["Adrian","Karen","Thanitos"] # Modelos o BD
+    return render_template(template_name_or_list="index.html", datos=base_de_datos) # Vista
