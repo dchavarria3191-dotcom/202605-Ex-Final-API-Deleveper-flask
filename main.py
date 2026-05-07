33e631cd0edee70ef07b4bd644a068820b0b1635
dchavarria3191-dotcom <dchavarri3191@gmail.com>
2026-05-07 18:50:25 -0500

    feat: Terminando Clase 3

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from flask import Flask, redirect, render_template, request, url_for, abort
+import sqlite3
 
 app = Flask(__name__)
+
+def get_db_connection():
+    conn = sqlite3.connect("basedatos.db")
+    conn.row_factory = sqlite3.Row
+    return conn
 
 
 @app.route("/")  # Controlador
@@ -8,14 +14,24 @@ def root():
     return render_template(template_name_or_list="base.html")  # Vista
 
 
-@app.route("/index")  # Controlador
-def index():
-    base_de_datos = ["Adrian", "Karen", "Thanitos"]  # Modelos o BD
-    return render_template(
-        template_name_or_list="index.html", datos=base_de_datos
-    )  # Vista
-
-
 @app.route("/home")  # Controlador
 def home():
-    return render_template(template_name_or_list="home.html")  # Vista
+    Connection = get_db_connection()
+    cur = Connection.cursor()
+    posts = cur.execute("SELECT * FROM posts")
+    return render_template(template_name_or_list="home.html", post_list=posts)  # Vista
+
+@app.route("/post/create", methods=("GET","POST"))
+def create_post():
+    if request.method == "GET":
+        return render_template("create.html")
+    if request.method == "POST":
+        title=request.form["title_title"]
+        content=request.form["content_content"]
+        connection = get_db_connection()
+        cur = connection.cursor()
+        cur.execute("INSERT INTO posts (title, content) values (?, ?)", (title, content))
+        connection.commit()
+        return redirect(url_for('home'))
+
+
