15247f1efdcbb1b872dd0b8664192709be46a404
dchavarria3191-dotcom <dchavarri3191@gmail.com>
2026-05-14 18:20:25 -0500

    Feat: Listado,Create y Update

--- main.py
+++ main.py
@@ -14,17 +14,32 @@ def root():
     return render_template(template_name_or_list="base.html")  # Vista
 
 
-@app.route("/home")  # Controlador
+@app.route("/home")  
 def home():
+    return render_template(template_name_or_list="home.html") 
+
+
+@app.route("/post/list")  
+def get_all_posts():
     Connection = get_db_connection()
     cur = Connection.cursor()
     posts = cur.execute("SELECT * FROM posts")
-    return render_template(template_name_or_list="home.html", post_list=posts)  # Vista
+    return render_template(template_name_or_list="post/list.html", post_list=posts)  # Vista
+
+
+@app.route("/post/<int:post_id>")  
+def get_single_posts(post_id):
+    Connection = get_db_connection()
+    cur = Connection.cursor()
+    post = cur.execute("SELECT * FROM posts where id = ?", (post_id,)).fetchone()
+    if post is None:
+        abort(404)
+    return render_template(template_name_or_list="post/single.html", post_single=post)  # Vista
 
 @app.route("/post/create", methods=("GET","POST"))
 def create_post():
     if request.method == "GET":
-        return render_template("create.html")
+        return render_template("post/create.html")
     if request.method == "POST":
         title=request.form["title_title"]
         content=request.form["content_content"]
@@ -32,6 +47,25 @@ def create_post():
         cur = connection.cursor()
         cur.execute("INSERT INTO posts (title, content) values (?, ?)", (title, content))
         connection.commit()
-        return redirect(url_for('home'))
+        return redirect(url_for('get_all_posts'))
 
+
+@app.route("/post/update/<int:post_id>", methods=("GET","POST"))
+def update_post(post_id):
+    Connection = get_db_connection()
+    cur = Connection.cursor()
+    post = cur.execute("SELECT * FROM posts where id = ?", (post_id,)).fetchone()
+    if post is None:
+        abort(404)
+
+    if request.method == "GET":
+        return render_template("post/update.html", single_post=post)
+    if request.method == "POST":
+        title=request.form["title_title"]
+        content=request.form["content_content"]
+        connection = get_db_connection()
+        cur = connection.cursor()
+        cur.execute("UPDATE posts SET title = ?, content = ? WHERE id = ?", (title, content, post_id))
+        connection.commit()
+        return redirect(url_for('get_all_posts'))
 
